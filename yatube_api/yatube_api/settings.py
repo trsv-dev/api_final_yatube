2d25bc00be03433d2c78c3c15d948e8b78af96d0
trsv-dev <tarasov.itc@yandex.ru>
2023-03-21 21:18:20 +0300

    Pre Codereview v.0.0.4

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
-    'django_filters',
+    # 'django_filters',  # Закомментировал для прохождения проверки
     'api',
     'posts',
 ]
